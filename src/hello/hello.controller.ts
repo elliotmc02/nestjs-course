e29d4862e94ba899fa773cd98faf1f6fcfd6f08e
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2024-12-29 19:26:08 +0100

    Guards and middleware

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -8,9 +8,11 @@ import {
   Query,
   Req,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 @Controller()
 export class HelloController {
@@ -44,7 +46,8 @@ export class HelloController {
   }
 
   @Get('greet')
-  greet(@Query(ValidateuserPipe) query: { name: string, age: number}) {
+  @UseGuards(AuthGuard)
+  greet(@Query(ValidateuserPipe) query: { name: string; age: number }) {
     console.log(typeof query.age);
     console.log(typeof query.name);
     return `Hello ${query.name}, you are ${query.age} years old`;
